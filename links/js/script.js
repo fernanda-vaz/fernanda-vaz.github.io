897e3dfccc215e76b44c1edce60cdd5dc1eb5321
Fernanda Vaz <flauta.fernanda@gmail.com>
2025-02-11 10:59:37 -0300

    upload novo design com react

--- links/js/script.js
+++
@@ -1,6 +0,0 @@
-const seeMoreBtn = document.getElementById("see-projects");
-const hideDiv = document.getElementById("hide-content");
-
-seeMoreBtn.addEventListener("click", () => {
-  hideDiv.classList.toggle("show");
-});
